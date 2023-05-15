862e00e56ca6726fd7f6d89ddb327a9aa02cda39
Isomorphism21 <isomorphismay1@gmail.com>
2023-05-15 09:20:30 -0500

    prueba 1 de php::rocket:

---
+++ intro.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        /* echo "<h1>titulo</h1>";
+
+        echo phpversion();
+
+        /* echo phpinfo; */
+        /* 1. Variables */ 
+
+        /* 2. errores */
+        // 2.1 Parse error: syntax error
+        $variable = "Hello Worlds";
+        echo $variable;
+
+        //2.2 warning
+        echo $variableasdasfasfa;
+        echo "Esto si se debe ejecutar"
+
+        //2.3 Fatal Error
+        
+
+    ?>
+
+</body>
+</html>
